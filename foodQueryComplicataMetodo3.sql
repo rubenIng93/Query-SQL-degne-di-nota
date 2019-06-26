cb2ef72724c58313d4d1c028e5ae8c398461b910
rubenIng93 <48275113+rubenIng93@users.noreply.github.com>
2019-06-26 19:17:03 +0200

    Update foodQueryComplicataMetodo3.sql
    Alla pressione del bottone ‘Ingredienti’ si costruisca un grafo
    pesato non orientato, i cui vertici rappresentino gli ingredienti
    sopra selezionati, ed i cui archi rappresentino il fatto che due
    ingredienti siano presenti nella composizione di almeno un cibo. Il
    peso dell’arco deve corrispondere al numero di cibi diversi
    (colonna ‘food_code’) che contengono entrambi gli ingredienti
    adiacenti all’arco stesso.
    
    La query estrapola i valori del punto B della simulazione del 12/06/2019 proposta del prof. Corno
    In questo caso la query è completa, nel senso che tramite una idMap precaricare , è possibile risalire a tutti gli ingredienti
    senza la necessità di usare un doppio ciclo for

--- foodQueryComplicataMetodo3.sql
+++ foodQueryComplicataMetodo3.sql
@@ -1,3 +1,5 @@
+Query SQL 12/06/2019
+
 SELECT fc1.condiment_food_code, fc2.condiment_food_code, COUNT(DISTINCT fc1.food_code)
 FROM food_condiment fc1, food_condiment fc2, condiment c1, condiment c2
 WHERE fc1.food_code = fc2.food_code
